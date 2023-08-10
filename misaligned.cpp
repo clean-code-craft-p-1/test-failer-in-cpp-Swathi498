658e9000571977c33e49f8f68c60cc94b32dfb74
Swathi498 <swatisri.r@philips.com>
2023-08-10 14:02:08 +0530

    ass3

--- misaligned.cpp
+++ misaligned.cpp
@@ -1,21 +1,42 @@
 #include <iostream>
 #include <assert.h>
 
+namespace ColorMap {
+
+int getColorPairNumber(int majorColorIndex, int minorColorIndex) {
+    return majorColorIndex * 5 + minorColorIndex;
+}
+
 int printColorMap() {
     const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
     const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
     int i = 0, j = 0;
     for(i = 0; i < 5; i++) {
         for(j = 0; j < 5; j++) {
-            std::cout << i * 5 + j << " | " << majorColor[i] << " | " << minorColor[i] << "\n";
+            std::cout << getColorPairNumber(i, j) << " | " << majorColor[i] << " | " << minorColor[i] << "\n";
         }
     }
     return i * j;
 }
+}
+
+namespace ColorMapTests {
+void TestPrintColorMap()
+{
+    int result = ColorMap::printColorMap();
+    assert(ColorMap::getColorPairNumber(0,0) == 1);
+    assert(ColorMap::getColorPairNumber(0,1) == 2);
+    assert(ColorMap::getColorPairNumber(0,2) == 3);
+    assert(ColorMap::getColorPairNumber(0,3) == 4);
+    assert(ColorMap::getColorPairNumber(0,4) == 5);
+    assert(ColorMap::getColorPairNumber(1,0) == 6);
+    assert(result == 25);
+}
+}
+
 
 int main() {
-    int result = printColorMap();
-    assert(result == 25);
+    ColorMapTests::TestPrintColorMap();
     std::cout << "All is well (maybe!)\n";
     return 0;
 }
